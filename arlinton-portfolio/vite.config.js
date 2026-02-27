f6f9bdf5b629c3c4ab2779e66a7e6f580fe24af5
Arlington Feliz <felizarlington@gmail.com>
2026-02-27 01:42:57 -0400

    feat: enhance CI/CD workflow and update Vite configuration for GitHub Pages deployment

--- arlinton-portfolio/vite.config.js
+++ arlinton-portfolio/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isUserSite = repository.endsWith('.github.io')
+const base = process.env.GITHUB_ACTIONS
+  ? isUserSite
+    ? '/'
+    : `/${repository}/`
+  : '/'
+
 export default defineConfig({
+  base,
   plugins: [react()],
 })
